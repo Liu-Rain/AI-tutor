6de64d88a0266b41263a7b111c2ea1e6b4a08da5
Ziqi Zhang <a1909438@adelaide.edu.au>
2025-04-01 10:57:51 +1030

    feat: :construction: integrate gemini into ai chatbox

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,38 @@
 import { useState } from "react";
 import { Chat } from "./components/Chat/chat";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 import styles from "./App.module.css";
 import { Controls } from "./components/Controls/Controls";
 
+const googlegenerativeai = new GoogleGenerativeAI(
+  import.meta.env.VITE_GOOGLE_AI_API_KEY
+);
+const gemini = googlegenerativeai.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+const chat = gemini.startChat({ history: [] });
+
 function App() {
   const [messages, setMessages] = useState([]);
-
-  function handleContentSend(content) {
+  function addMessage(message) {
+    setMessages((prevMessages) => [...prevMessages, message]);
+  }
+  async function handleContentSend(content) {
+    addMessage({ content, role: "user" });
     setMessages((previousMessages) => [
       ...previousMessages,
       { content, role: "user" },
     ]);
+
+    try {
+      const result = await chat.sendMessage(content);
+      addMessage({
+        content: result.response.text(),
+        role: "assistant",
+      });
+    } catch (error) {
+      addMessage({ content: "Error!", role: "system" });
+    }
   }
   return (
     <div className={styles.App}>
